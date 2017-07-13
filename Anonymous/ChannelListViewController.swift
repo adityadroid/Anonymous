f134577af0f8252ac15850cc3868c0f1b686faec
Aditya Gurjar <adityaadi1467@gmail.com>
2017-07-13 11:49:40 +0530

    Initial commit

---
+++ Anonymous/ChannelListViewController.swift
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2015 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import Firebase
+
+enum Section: Int {
+    case createNewChannelSection = 0
+    case currentChannelsSection
+}
+class ChannelListViewController: UITableViewController {
+    var senderDisplayName: String?
+    var newChannelTextField: UITextField?
+    private var channels: [Channel] = []
+    private lazy var channelRef: DatabaseReference = Database.database().reference().child("channels")
+    private var channelRefHandle: DatabaseHandle?
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+   
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let currentSection: Section = Section(rawValue: section) {
+            switch currentSection {
+            case .createNewChannelSection:
+                return 1
+            case .currentChannelsSection:
+                return channels.count
+            }
+        } else {
+            return 0
+        }
+    }
+    
+    // 3
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let reuseIdentifier = (indexPath as NSIndexPath).section == Section.createNewChannelSection.rawValue ? "NewChannel" : "ExistingChannel"
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        
+        if (indexPath as NSIndexPath).section == Section.createNewChannelSection.rawValue {
+            if let createNewChannelCell = cell as? CreateChannelCell {
+                newChannelTextField = createNewChannelCell.newChannelNameField
+            }
+        } else if (indexPath as NSIndexPath).section == Section.currentChannelsSection.rawValue {
+            cell.textLabel?.text = channels[(indexPath as NSIndexPath).row].name
+        }
+        
+        return cell
+    }
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, "10.3"
+use_frameworks!
+
+target 'Anonymous' do
+  pod 'Firebase/Storage'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'JSQMessagesViewController'
+
+end
